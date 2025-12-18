4643d7a00f3cbf8e2ae4e64c3affe5d8f0c8cfa1
mdreja09 <muhammadreja0099@gmail.com>
2025-12-18 23:48:20 +0600

    sign in

--- lib/view/authentication/register/ui.dart
+++ lib/view/authentication/register/ui.dart
@@ -1,3 +1,4 @@
+import 'package:dada_ecommerce/widget/appbar/appbar.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -11,13 +12,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF0F5F9),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xffFFFFFF),
 
         leading: InkWell(
           focusColor: Colors.green,
           onTap: () {
-            setState(() {});
+            Navigator.pop(context);
 
             //Navigator.pop(context,MaterialPageRoute(builder: (context)=>LoginScreen()));
           },
@@ -30,6 +32,65 @@ class _RegisterScreenState extends State<RegisterScreen> {
         centerTitle: true,
         title: Image.asset(height: 30, width: 133, "assets/image/dadaapp.png"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Container(
+              height: 1,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+
+              ),
+
+
+
+            ),
+             Row(
+               spacing: 20,
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: [
+
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Text("Sign in",style: TextStyle(fontSize: 25),
+                        ),
+                        Container(height: 2,color: Colors.orange,
+                            width: MediaQuery.sizeOf(context).width/2,)
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+
+                      children: [
+                        Text("Sign in",style: TextStyle(fontSize: 25),),
+                        Container(height: 2,color: Colors.blue,
+                          width: MediaQuery.sizeOf(context).width/2,)
+                      ],
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+
+          ],
+        ),
+      ),
+
+
+
     );
   }
 }
