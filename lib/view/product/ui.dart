aee46c81eca84f48a46093337174ba862d486520
mdreja09 <muhammadreja0099@gmail.com>
2026-01-02 22:27:10 +0600

    offer Image

--- lib/view/product/ui.dart
+++ lib/view/product/ui.dart
@@ -23,69 +23,80 @@ class _ProductScreenState extends State<ProductScreen> {
         //title: Center(child: Text("Data"),) ,
         // title: Image.asset(height: 20, width: 120, "assets/image/w_logo.png"),
       ),
+
       body: GridView.builder(
         padding: EdgeInsetsGeometry.all(10),
 
         itemCount: 5,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 0.9,
+          childAspectRatio: 0.8,
           crossAxisCount: 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 8,
         ),
 
-        itemBuilder: (context, index) => Card(
-          child: Column(
-            children: [
-              Container(
-                height: 156,
-                width: 173,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(10),
-                    topLeft: Radius.circular(10),
+        itemBuilder: (context, index) => Stack(
+          children: [
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    height: 156,
+                    width: 173,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(10),
+                      ),
+                    ),
+                    child: Image.asset(fit: BoxFit.fill, "assets/image/sari.png"),
                   ),
-                ),
-                child: Image.asset(fit: BoxFit.fill, "assets/image/sari.png"),
-              ),
-              CustomTextWidget(
-                text: "Party Borkha Abaya Koliza",
-                fSize: 14,
-                fW: FontWeight.w500,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25),
-                child: Row(
-
-                  children: [
-                    CustomTextWidget(
-                      text: "2800",
-                      fW: FontWeight.w500,
-                      fSize: 18,
+                  CustomTextWidget(
+                    text: "Party Borkha Abaya Koliza",
+                    fSize: 16,
+                    fW: FontWeight.w500,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 21),
+                    child: Row(
+                      children: [
+                        CustomTextWidget(
+                          text: "2800",
+                          fW: FontWeight.w500,
+                          fSize: 18,
+                        ),
+                        SizedBox(width: 5),
+                        CustomTextWidget(
+                          text: "3200",
+                          fSize: 14,
+                          fW: FontWeight.w500,
+                          tD: TextDecoration.lineThrough,
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 5,),
-                    CustomTextWidget(
-                      text: "3200",
-                      fSize: 14,
-                      fW: FontWeight.w500,
-                      tD: TextDecoration.lineThrough,
+                  ),
+                  SizedBox(height: 6),
+                  Container(
+                    height: 30,
+                    width: 85,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                  ],
-                ),
+                    child: Center(
+                      child: Text(
+                        "Add to Card",
+                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                
-                height: 30,
-              width: 85,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(5)
-              ),
-                child: Center(child: Text("Add to Card",
-                  style: TextStyle(fontSize: 12,fontWeight: FontWeight.w500),),),
-              )
-            ],
-          ),
+            ),
+            Positioned(
+              left: 26,
+                child: Image.asset("assets/image/offer.png"))
+          ],
         ),
       ),
     );
